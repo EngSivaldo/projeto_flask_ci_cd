36f3640b68828f644cb6c319592a52d8ddeedab2
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-11-18 16:28:49 -0300

    Nova feature: mensagem atualizada

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "CI/CD com Flask funcionando!"
+    return "Nova Versão do Site: CI/CD Funciona Mesmo!"
 
 if __name__ == "__main__":
     app.run(debug=True)
